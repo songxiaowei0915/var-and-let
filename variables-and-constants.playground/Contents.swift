fa0b276a994bb715513e1967255d6880be4809ca
宋小伟 <songxiaowei@songxiaoweideMacBook-Pro.local>
2022-09-21 19:28:13 +0800

    add variables-and-constants.playground

---
+++ variables-and-constants.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var swiftString = "Swift is fun"
+
+var hours = 24
+
+var PI = 3.14
+
+var swiftIsFun = true
+
+var me = ("Mars", 11, "11@boxue.io")
+me.0
+me.1
+me.2
+
+var x: Int
+var s: String
+
+PI = 3.1415
+
+let minutes = 30
+let fireIsHot = true
